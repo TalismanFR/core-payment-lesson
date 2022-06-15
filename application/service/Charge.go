907074ce67279fc3e5f7bd3d3e436b66ab49d1cd
Roman Ukhobotov <ukhobotov.rv@phystech.edu>
2022-06-15 10:29:33 +0300

    handle errors

--- application/service/Charge.go
+++ application/service/Charge.go
@@ -1,36 +1,40 @@
 package service
 
 import (
-	"diLesson/application"
-	"diLesson/application/contract/dto"
-	"diLesson/application/domain"
-	contract2 "diLesson/payment/contract"
-	"github.com/golobby/container/v3"
-	uuid "github.com/satori/go.uuid"
-	"time"
+    "diLesson/application"
+    "diLesson/application/contract/dto"
+    "diLesson/application/domain"
+    contract2 "diLesson/payment/contract"
+    "github.com/golobby/container/v3"
+    uuid "github.com/satori/go.uuid"
+    "time"
 )
 
 type Charge struct {
-	repository application.PayRepository `container:"type"`
+    repository application.PayRepository `container:"type"`
 }
 
 func (c Charge) Charge(request dto.ChargeRequest) (*dto.ChargeResult, error) {
 
-	pay := domain.NewPay(uuid.NewV4(), request.Amount, "RUB", request.InvoiceId, 0, "new", time.Now(), "")
-	//todo получение информации о вендоре
+    pay := domain.NewPay(uuid.NewV4(), request.Amount, "RUB", request.InvoiceId, 0, "new", time.Now(), "")
+    //todo получение информации о вендоре
 
-	var service contract2.VendorCharge
-	container.NamedResolve(&service, "bepaid_charge")
+    var service contract2.VendorCharge
+    if err := container.NamedResolve(&service, "bepaid_charge"); err != nil {
+        return nil, err
+    }
 
-	result, err := service.Charge(pay)
-	if err != nil {
-		return nil, err
-	}
+    result, err := service.Charge(pay)
+    if err != nil {
+        return nil, err
+    }
 
-	pay.HandleChargeResult(result)
+    pay.HandleChargeResult(result)
 
-	c.repository.Save(pay)
+    if err := c.repository.Save(pay); err != nil {
+        return nil, err
+    }
 
-	r := dto.NewChargeResult(0, "success", pay.Uuid().String())
-	return r, nil
+    r := dto.NewChargeResult(0, "success", pay.Uuid().String())
+    return r, nil
 }
