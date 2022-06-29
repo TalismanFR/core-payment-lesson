a8e7d8a534828bdf52be6255f0baa16400cb5cde
tim <timaz98@ya.ru>
2022-06-29 21:22:03 +0500

    add entry point for application

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,61 @@
 package main
 
 import (
+	"diLesson/application/contract"
+	"diLesson/config"
 	"diLesson/server"
+	"github.com/golobby/container/v3"
+	"google.golang.org/grpc"
 	"log"
+	"net"
+	"path/filepath"
 )
 
 func main() {
-	if err := server.Run(); err != nil {
+
+	log.Println("config: start")
+
+	p, err := filepath.Abs("configs/main.yaml")
+	if err != nil {
 		log.Fatal(err)
 	}
+
+	conf, err := config.Parse(p)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("config: end")
+
+	// TODO:sometimes test fail without time.Sleep
+	//time.Sleep(5 * time.Second)
+
+	log.Println("building dependencies: start")
+	err = config.BuildDI(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var service contract.Charge
+	err = container.Resolve(&service)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("building dependencies: end")
+
+	s := server.NewServer(service)
+	ls, err := net.Listen("tcp", ":50051")
+
+	gs := grpc.NewServer()
+	server.RegisterPayServiceServer(gs, s)
+
+	log.Println("server: start")
+
+	if err = gs.Serve(ls); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("server: end")
 }
