3ef8a896476b02b602d67e5eda1f3ae06079953f
tim <timaz98@ya.ru>
2022-07-01 23:11:46 +0500

    charge: add context to args

--- application/contract/charge.go
+++ application/contract/charge.go
@@ -1,10 +1,11 @@
 package contract
 
 import (
+	"context"
 	"diLesson/application/contract/dto"
 )
 
 type Charge interface {
 	//TODO: add context
-	Charge(request dto.ChargeRequest) (*dto.ChargeResult, error)
+	Charge(ctx context.Context, request dto.ChargeRequest) (*dto.ChargeResult, error)
 }
